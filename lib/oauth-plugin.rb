4a463d04f205cdf6a8e94de138af0a7aefe47fb0
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2021-03-09 13:43:26 +0900

    drop support old rails version

--- lib/oauth-plugin.rb
+++ lib/oauth-plugin.rb
@@ -3,21 +3,13 @@ require 'oauth/signature/hmac/sha1'
 require 'oauth/request_proxy/rack_request'
 require 'oauth/server'
 require 'oauth/controllers/application_controller_methods'
-if Rails::VERSION::MAJOR >= 3
-  require 'oauth/request_proxy/rack_request'
-else
-  require 'oauth/request_proxy/action_controller_request'
-  ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
-end
+require 'oauth/request_proxy/rack_request'
 
-
-if Rails::VERSION::MAJOR >= 3
-  module OAuth
-    module Plugin
-      class OAuthRailtie < Rails::Railtie
-        initializer "oauth-plugin.configure_rails_initialization" do |app|
-          ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
-        end
+module OAuth
+  module Plugin
+    class OAuthRailtie < Rails::Railtie
+      initializer "oauth-plugin.configure_rails_initialization" do |app|
+        ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
       end
     end
   end
